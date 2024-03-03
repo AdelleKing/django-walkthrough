c222d092356343a35b7e64b74738ff85fb3097ff
Adelle King <adelle_xx@yahoo.co.uk>
2024-03-03 19:14:27 +0000

    added sorting functionality

--- products/views.py
+++ products/views.py
@@ -22,6 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey ='lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -33,7 +35,7 @@ def all_products(request):
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            ctegories = Category.objects.filter(name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
 
         if 'q' in request.GET:
