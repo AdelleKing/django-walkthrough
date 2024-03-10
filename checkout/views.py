7990b6e759e431ef0026a57bf3253d9c3267d256
Adelle King <adelle_xx@yahoo.co.uk>
2024-03-10 19:55:51 +0000

    addes tripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OsrwCB1lwt51R3Z0jnlIkuXRqajJ7phsYIA85TkaxDGIQHTuTy152LVIZ4C4DcDrytVPYVds2mqAMRjOSO2Mtck00I5Fi5b8I',
+        'client_secret': ' test client secret',
     }
 
     return render(request, template, context)
