5340e0be115bb113d0b985c5a48246b9932c4bfd
Adelle King <adelle_xx@yahoo.co.uk>
2024-03-14 11:58:26 +0000

    re-installed packages

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-adelleking-djangowalkth-atxhstrdqlw.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-adelleking-djangowalkth-chkpwaist0m.ws-eu110.gitpod.io']
 
 
 # Application definition
