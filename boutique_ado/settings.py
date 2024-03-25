ad1ba4007ef7e051296a6058c2510fb4a607a4b0
Adelle King <adelle_xx@yahoo.co.uk>
2024-03-25 13:22:42 +0000

    edits

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-walkthrough-adelle.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-adelleking-djangowalkth-6iu9cqnvp4x.ws-eu110.gitpod.io']
 
 
 # Application definition
